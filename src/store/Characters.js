cb729d87ecd77e2065bdb4d5720f7f71f77b95ff
leahcarlin <leahrcarlin@gmail.com>
2025-02-11 17:57:19 +0100

    cleanup

--- src/store/Characters.js
+++ src/store/Characters.js
@@ -9,50 +9,38 @@ export class Characters {
   }
 
   getRandomCharacter() {
-    try {
-      // filter out characters that have already been used
-      const availableCharacters = this.characters.filter(
-        (character) => !this.usedCharacterIds.includes(character._id)
-      );
+    // filter out characters that have already been used
+    const availableCharacters = this.characters.filter(
+      (character) => !this.usedCharacterIds.includes(character._id)
+    );
 
-      if (availableCharacters.length === 0) {
-        throw new Error("No available characters left to select");
-      }
-
-      // pick a random character from the available list
-      const randomIndex = Math.floor(
-        Math.random() * availableCharacters.length
-      );
-      return availableCharacters[randomIndex];
-    } catch (e) {
-      console.error(e.message);
+    if (availableCharacters.length === 0) {
+      throw new Error("No available characters left to select");
     }
+
+    // pick a random character from the available list
+    const randomIndex = Math.floor(Math.random() * availableCharacters.length);
+    return availableCharacters[randomIndex];
   }
 
   getRandomChoices(type, id) {
     const numChoices = 3;
     const choices = new Set();
 
-    try {
-      while (choices.size < numChoices) {
-        const choice = this.getRandomCharacter([id]);
-        const currentCharacter = this.characters.find(
-          (char) => char._id === id
-        );
+    while (choices.size < numChoices) {
+      const choice = this.getRandomCharacter([id]);
+      const currentCharacter = this.characters.find((char) => char._id === id);
 
-        // ensure no duplicates
-        if (
-          choice._id !== id &&
-          !choices.has(choice[type][0]) &&
-          choice[type][0] !== currentCharacter[type][0]
-        ) {
-          choices.add(choice[type][0]);
-        }
+      // ensure no duplicates
+      if (
+        choice._id !== id &&
+        !choices.has(choice[type][0]) &&
+        choice[type][0] !== currentCharacter[type][0]
+      ) {
+        choices.add(choice[type][0]);
       }
-      return Array.from(choices); // convert Set back to Array before returning
-    } catch (e) {
-      console.error(e.message);
     }
+    return Array.from(choices); // convert Set back to Array before returning
   }
 
   addUsedCharacterId(id) {
